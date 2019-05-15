a54a7d62913a9b4ab6290ce793c8542341a2dc30
Tony <tony.krikorian@outlook.com>
2019-05-14 21:14:50 -0400

    EF CodeFirst From Database

---
+++ EF6.CodeFirstFromDB.Console/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace EF6.CodeFirstFromDB.Console.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EF6.CodeFirstFromDB.Console.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EF6.CodeFirstFromDB.Console.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ EF6.Demo.Console/PlutoContext.cs
@@ -0,0 +1,21 @@
+﻿using EF6.Demo.Console.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6.Demo.Console
+{
+    class PlutoContext: DbContext
+    {
+        public PlutoContext():base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
